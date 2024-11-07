d4d9d7c0d2e8151f72a9c601dd0dda78739ba297
hzargar2 <hzargar2@gmail.com>
2024-11-07 17:30:01 -0500

    added selections and functions to filter for trades

--- pages/clients/[id]/trades/index.vue
+++ pages/clients/[id]/trades/index.vue
@@ -1,12 +1,91 @@
 <script setup lang="ts">
 
-const { data } = await useFetch('https://paradigmapi.pythonanywhere.com/api/clients')
+// interface Trade{
+//     ticker: string,
+//     full_ticker: string,
+//     id: string,
+//     client: string,
+//     date: string,
+//     side: string,
+//     quantity: number,
+//     price: number,
+//     commission_type: string,
+//     commission_amount: number,
+// }
+
+const router = useRouter();
+const id = router.currentRoute.value.params.id;
+const { data: trades } = await useFetch(`https://paradigmapi.pythonanywhere.com/api/clients/${id}/trades`);
+
+let tickers = [...new Set(trades.value.map((value) => value['full_ticker']))];
+let selected_ticker= ref(null);
+
+let sides = [...new Set(trades.value.map((value) => value['side']))];
+let selected_side= ref(null);
+
+let commission_types = [...new Set(trades.value.map((value) => value['commission_type']))];
+let selected_commission_type= ref(null);
+
+let selected_trades = trades.value;
+
+const filterTrades = (event) => {
+
+    let filtered_trades = trades.value;
+    if (selected_side.value !== null){
+        filtered_trades = filtered_trades.filter((trade) => trade['side'] === selected_side.value);
+    }
+    if (selected_ticker.value !== null){
+        filtered_trades = filtered_trades.filter((trade) => trade['full_ticker'] === selected_ticker.value);
+    }
+    if (selected_commission_type.value !== null){
+        filtered_trades = filtered_trades.filter((trade) => trade['commission_type'] === selected_commission_type.value);
+    }
+
+    selected_trades = filtered_trades;
+
+    console.log(selected_trades)
+}
+
+
+
 
 </script>
 
 <template>
-  <div>
-      {{ $route.params.id }}
+  <div class="flex flex-col w-full h-full space-y-8">
+      <span class="mx-auto ml-0 text-3xl font-medium">Trades</span>
+      <div class="flex w-full h-full space-x-24">
+          <div class="flex flex-col w-1/4 h-full">
+
+              <form class="max-w-sm">
+                  <label for="selected_ticker" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ticker</label>
+                  <select @change="filterTrades($event)" v-model="selected_ticker" id="selected_ticker" class="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                      <option selected disabled :value="null">Choose a ticker</option>
+                      <option v-for="item in tickers" :value="item">{{item}}</option>
+                  </select>
+              </form>
+
+              <form class="max-w-sm">
+                  <label for="selected_side" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Side</label>
+                  <select @change="filterTrades($event)" v-model="selected_side" id="selected_side" class="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                      <option selected disabled :value="null">Choose side</option>
+                      <option v-for="item in sides" :value="item">{{item}}</option>
+                  </select>
+              </form>
+
+              <form class="max-w-sm">
+                  <label for="selected_commission_type" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Commission Type</label>
+                  <select @change="filterTrades($event)" v-model="selected_commission_type" id="selected_commission_type" class="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                      <option selected disabled :value="null">Choose commission type</option>
+                      <option v-for="item in commission_types" :value="item">{{item}}</option>
+                  </select>
+              </form>
+
+          </div>
+          <div class="flex w-3/4 h-full border border-gray-200">
+            asas
+          </div>
+      </div>
   </div>
 
 </template>
