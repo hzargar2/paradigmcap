5b075bc67bd8847ff84f4ac489b223fc2280be97
hzargar2 <hzargar2@gmail.com>
2024-11-07 00:44:27 -0500

    improved styling

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
         <nav class="w-full flex relative bg-white drop-shadow-md z-[2]">
             <div class="flex p-4 px-5 w-full justify-between mx-auto text-sm">
                 <a href="/" class="w-auto flex items-center">
-                    <img src="~/assets/logo-paradigm-dark.png" class="h-11" alt="Paradigm Capital Logo" />
+                    <img src="~/assets/logo-paradigm-dark.png" class="h-10" alt="Paradigm Capital Logo" />
                 </a>
             </div>
         </nav>
