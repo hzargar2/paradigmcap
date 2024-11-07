f3c36df4f33fe5e9014b194f963d4bdf19ed54f1
hzargar2 <hzargar2@gmail.com>
2024-11-07 00:38:27 -0500

    added sidebar skeleton, both ides can scroll independently now

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
+    <div class="flex flex-col h-screen w-full">
+        <nav class="w-full flex relative bg-white drop-shadow-md z-[2]">
+            <div class="flex p-4 px-5 w-full justify-between mx-auto text-sm">
+                <a href="/" class="w-auto flex items-center">
+                    <img src="~/assets/logo-paradigm-dark.png" class="h-12" alt="Paradigm Capital Logo" />
+                </a>
+            </div>
+        </nav>
         <!-- Markup shared across all pages, ex: NavBar -->
         <NuxtPage />
     </div>
