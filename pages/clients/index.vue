73638a52680072d91a662558e9057b887c5ee429
hzargar2 <hzargar2@gmail.com>
2024-11-08 19:15:01 -0500

    disable enter in search

--- pages/clients/index.vue
+++ pages/clients/index.vue
@@ -19,7 +19,7 @@ let filterClients = () => {
 <template>
     <div class="flex flex-col w-full h-full space-y-8 p-6">
 
-        <form class="max-w-lg mx-auto w-full">
+        <form class="max-w-lg mx-auto w-full" onkeydown="return event.key !== 'Enter'">
             <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div class="relative">
                 <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
