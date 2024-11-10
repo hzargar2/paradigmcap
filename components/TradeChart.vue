441bac751235a1f3fda2863a93de1ebb48e161ba
hzargar2 <hzargar2@gmail.com>
2024-11-10 17:31:37 -0500

    improved styling

--- components/TradeChart.vue
+++ components/TradeChart.vue
@@ -7,6 +7,8 @@ ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 let props = defineProps(['trades']);
 
+console.log(props.trades);
+
 let tickers = [...new Set(props.trades.map((value) => value['full_ticker']))];
 let selected_ticker= ref(null);
 
@@ -16,13 +18,22 @@ let selected_side= ref(null);
 let commission_types = [...new Set(props.trades.map((value) => value['commission_type']))];
 let selected_commission_type= ref(null);
 
-let total_commission = props.trades.map((trade) => {
-    return trade['commission'];
-}).reduce((pv, cv) => { return pv + cv; }, 0);
+let total_volume = ref(props.trades.map((trade) => {
+    return trade['quantity'] * parseFloat(trade['price']);
+}).reduce((pv, cv) => { return pv + cv; }, 0));
 
-let average_commission = Math.round(((props.trades.map((trade) => {
+let total_commission = ref(props.trades.map((trade) => {
     return trade['commission'];
-}).reduce((pv, cv) => { return pv + cv; }, 0) / props.trades.length) + Number.EPSILON) * 100) / 100
+}).reduce((pv, cv) => { return pv + cv; }, 0));
+
+let average_commission_per_trade = ref(Math.round(((props.trades.map((trade) => {
+    return trade['commission'];
+}).reduce((pv, cv) => { return pv + cv; }, 0) / props.trades.length) + Number.EPSILON) * 100) / 100)
+
+let average_commission_per_share = ref(Math.round(((props.trades.map((trade) => {
+    return trade['commission'] / trade['quantity'];
+}).reduce((pv, cv) => { return pv + cv; }, 0) / props.trades.length) + Number.EPSILON) * 1000000) / 1000000)
+
 
 let series = ref([
     {
@@ -109,8 +120,6 @@ let chartOptions = ref({
 
 const filterTrades = (event) => {
 
-    console.log(selected_ticker);
-
     let filtered_trades = props.trades;
     if (selected_side.value !== null){
         filtered_trades = filtered_trades.filter((trade) => trade['side'] === selected_side.value);
@@ -172,10 +181,21 @@ const filterTrades = (event) => {
         }
     ]
 
+    total_volume.value = filtered_trades.map((trade) => {
+        return trade['quantity'] * parseFloat(trade['price']);
+    }).reduce((pv, cv) => { return pv + cv; }, 0);
 
-    console.log(chartOptions.value)
-    console.log(series.value)
+    total_commission.value = filtered_trades.map((trade) => {
+        return trade['commission'];
+    }).reduce((pv, cv) => { return pv + cv; }, 0);
 
+    average_commission_per_trade.value = Math.round(((filtered_trades.map((trade) => {
+        return trade['commission'];
+    }).reduce((pv, cv) => { return pv + cv; }, 0) / filtered_trades.length) + Number.EPSILON) * 100) / 100
+
+    average_commission_per_share.value = Math.round(((filtered_trades.map((trade) => {
+        return trade['commission'] / trade['quantity'];
+    }).reduce((pv, cv) => { return pv + cv; }, 0) / filtered_trades.length) + Number.EPSILON) * 1000000) / 1000000
 
 }
 
@@ -186,8 +206,26 @@ const filterTrades = (event) => {
 <template>
 
     <div class="w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
-        <div class="flex justify-between">
-            <div class="flex flex-row space-x-6">
+        <div class="flex justify-between space-x-4">
+            <div class="flex justify-between space-x-6">
+                <div>
+                    <h5 class="leading-none text-center text-lg font-bold text-gray-900 dark:text-white pb-2">${{total_volume}}</h5>
+                    <p class="text-base font-normal text-center text-gray-500 dark:text-gray-400">Total Volume</p>
+                </div>
+                <div>
+                    <h5 class="leading-none text-center text-lg font-bold text-gray-900 dark:text-white pb-2">${{total_commission}}</h5>
+                    <p class="text-base font-normal text-center text-gray-500 dark:text-gray-400">Total Commission</p>
+                </div>
+                <div>
+                    <h5 class="leading-none text-center text-lg font-bold text-gray-900 dark:text-white pb-2">${{average_commission_per_trade}}</h5>
+                    <p class="text-base font-normal text-center text-gray-500 dark:text-gray-400">Avg. Commission / Trade</p>
+                </div>
+                <div>
+                    <h5 class="leading-none text-center text-lg font-bold text-gray-900 dark:text-white pb-2">${{average_commission_per_share}}</h5>
+                    <p class="text-base font-normal text-center text-gray-500 dark:text-gray-400">Avg. Commission / Share</p>
+                </div>
+            </div>
+            <div class="flex flex-row space-x-4">
                 <form class="max-w-sm">
                     <select @change="filterTrades($event)" v-model="selected_ticker" id="selected_ticker" class="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500">
                         <option selected :value="null">All Tickers</option>
